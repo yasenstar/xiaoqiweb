47447c2413711c249fd4b161975f478bf5cfc687
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-07 22:00:00 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="EA-logo" alt="logo" />
         <p>
-          Hello
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Xiaoqi's Home Site
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.udemy.com/user/xiaoqi-zhao/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn my courses in Udemy
         </a>
       </header>
     </div>
