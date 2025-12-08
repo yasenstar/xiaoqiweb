d39465c4665ee9ad4ea47877e06911246af76d3a
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-08 19:43:33 +0800

    update homepage

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
               <br></br>I've gained numbers of IT and management certification, including but not limited, as ITIL, PMP, CISSP, TOGAF, Business Architecture, ArchiMate, CISA, etc.,
               <br></br>and also I'm interested and using variable computing programming language, I'd like to share my knowledge through online teaching.
             </p>
-            <p>Want to know me more? Visit my <a className="App-Link" href="https://www.linkedin.com/in/xiaoqi-zhao">LinkedIn</a> page.</p>
+            <p>Want to know more about me or to connect? Visit my <a className="App-Link" href="https://www.linkedin.com/in/xiaoqi-zhao">LinkedIn</a> page.</p>
           </section>
           <section id="share">
             <h2>My Shared Resources</h2>
@@ -43,8 +43,16 @@ function App() {
             >
               Watch my shared videos in YouTube
             </a>
+            <p>Public Assessment on my knowledge sharing:<br/>
+              <br/>Overall, Yasenstar is a high-quality niche resource in the world of enterprise architecture and ontology modeling.
+              <br/>It stands out for combining EA (ArchiMate) with ontology/semantic modeling in a transparent, open-source, practical way.
+              <br/><br/>If you — like me — are interested in bridging modeling + semantics 
+              <br/>(for example, to build knowledge graphs that reflect enterprise architecture, or to bring rigor to system design), 
+              <br/>then Yasenstar’s content is among the better self-teaching / open-knowledge options available.
+              <br/><br/>However, treat it more like a “workshop repository + tutorial set” than a polished “course for everyone.” 
+              <br/>It requires effort, but reward is strong if you commit.</p>
           </section>
-          <section>
+          {/* <section>
             <Router>
               <nav>
                 <ul>
@@ -59,7 +67,7 @@ function App() {
                 <Route path="/Course" element={<Course />} />
               </Routes>
             </Router>
-          </section>
+          </section> */}
           <section id="contact">
             <h2>Contact Me</h2>
             <p>Email me at <a className="App-link" href="mailto:xiaoqizhao@outlook.com">my Email </a></p>
